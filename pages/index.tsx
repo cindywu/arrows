d4edfdeaad794bcedd2e90a14fc1e0301a0465e8
cindy <1177031+cindywu@users.noreply.github.com>
2023-01-24 13:28:43 -1000

    lettuce begin

---
+++ pages/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div className={"mt-20 text-center"}>
+      <div className={"text-8xl"}>⤏</div>
+      <div className={"font-mono text-sm text-zinc-400"}>arrows</div>
+    </div>
+  )
+}
